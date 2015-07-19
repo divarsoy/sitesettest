907cf2f618f8e22da79b04579c782f87cf6d9ad8
Dag Ivarsoy <dagivarsoy@gmail.com>
2015-07-19 14:30:02 +0100

    add schema for clients

---
+++ database/migrations/2015_07_19_131122_create_clients_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('address');
+            $table->string('phone');
+            $table->string('email');
+            $table->boolean('email_notification')->default(true);
+            $table->boolean('sms_notification')->default(true);
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('clients');
+    }
+}
